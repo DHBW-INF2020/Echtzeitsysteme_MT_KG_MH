f9112488c0e13b1d5aa48694e999595f19ae9a8f
Maurice Tonn <inf20127@lehre.dhbw-stuttgart.de>
2023-03-10 09:17:12 +0100

    ADD: Step-Function in Logic.JS

--- testerino/logic.js
+++ testerino/logic.js
@@ -3,20 +3,34 @@ const StepButton = document.getElementById('step');
 function Step(){
     Actions.forEach(element => 
         {
+            console.log(element)
             if(element.isrunnuing === true)
-            {
-                console.log(element.actionID.toString() + " is running")
+            { 
+                console.log(element.name.toString() + " is running")
+                if(element.steps === element.currentSteps)
+                {
+                    console.log(element.name.toString() + " just finished")
+                    element.stop()
+                    element.releaseRessources()
+                }
+                else
+                {
+                    console.log(element.name.toString() + " takes Step" + element.currentSteps.toString())
+                    element.takeStep()
+                }              
             }
             else
             {
-                console.log(element.actionID.toString() + " is not running")
-                if(element.checkRessources() === true)
+                console.log(element.name.toString() + " is not running")
+                if(element.checkResources() === true)
                 {
+                    console.log(element.name.toString() + " starts now")
+                    element.takeRessources()
                     element.startAction()
                 }
                 else
                 {
-                    None
+                    
                 }
             }
         })
